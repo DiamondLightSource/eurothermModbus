a350c602965da6e62f993df5efe30baa9bbf0d9f
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2018-08-14 14:57:33 +0100

    Follow builder object naming convention

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,14 @@
+from iocbuilder import AutoSubstitution, Xml
 
+
+class eurotherm3504(Xml):
+        TemplateFile = "eurotherm3504.xml"
+
+
+class eurotherm3504Archive(AutoSubstitution):
+        TemplateFile = "eurotherm3504-archive.template"
+
+
+class eurotherm3504Gui(AutoSubstitution):
+        TemplateFile = "eurotherm3504-gui.template"
 
