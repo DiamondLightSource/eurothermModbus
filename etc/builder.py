93cb116d77ce2485662e1651ba74d7d2276670eb
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2018-11-16 18:09:28 +0000

    Refactor to avoid repeating and add loop DISABLE alias

--- etc/builder.py
+++ etc/builder.py
@@ -1,26 +1,118 @@
-from iocbuilder import AutoSubstitution, Xml
+from iocbuilder import AutoSubstitution
+from iocbuilder import Device
+from iocbuilder import Xml
+from iocbuilder.modules.modbus import modbusPort
+from iocbuilder.modules.modbus import modbusInterpose
+from iocbuilder.modules.asyn import AsynIP
+from iocbuilder.arginfo import *
 
 
-class eurotherm3504(Xml):
-        TemplateFile = "eurotherm3504.xml"
+class _EurothermDisable(AutoSubstitution):
+    TemplateFile = "eurothermDis.template"
 
 
-class eurotherm3504Archive(AutoSubstitution):
-        TemplateFile = "eurotherm3504-archive.template"
+class _EurothermL1(AutoSubstitution):
+    TemplateFile = "eurothermL1.template"
 
 
-class eurotherm3504Gui(AutoSubstitution):
-        TemplateFile = "eurotherm3504-gui.template"
+class _EurothermL2(AutoSubstitution):
+    TemplateFile = "eurothermL2.template"
 
 
-class eurotherm2k(Xml):
-        TemplateFile = "eurotherm2k.xml"
+class _EurothermProg(AutoSubstitution):
+    TemplateFile = "eurothermProg.template"
 
 
-class eurotherm2kArchive(AutoSubstitution):
-        TemplateFile = "eurotherm2k-archive.template"
+class _EurothermRec(AutoSubstitution):
+    TemplateFile = "eurothermRec.template"
 
 
-class eurotherm2kGui(AutoSubstitution):
-        TemplateFile = "eurotherm2k-gui.template"
+class _Eurotherm2kGui(AutoSubstitution):
+    TemplateFile = "eurotherm2k-gui.template"
 
+
+class _Eurotherm3kGui(AutoSubstitution):
+    TemplateFile = "eurotherm3k-gui.template"
+
+
+class _Eurotherm2kArchive(AutoSubstitution):
+    TemplateFile = "eurotherm2k-archive.template"
+
+
+class _Eurotherm3kArchive(AutoSubstitution):
+    TemplateFile = "eurotherm3k-archive.template"
+
+
+class _Eurotherm(Device):
+    linktype = "tcpip"
+
+    def __init__(self, name, port, device, ipaddress, tcpport, rregu="C/s",
+                 modbus_prefix="MB1", noAutoConnect=False):
+
+        self.__dict__.update(locals())
+        dest_address = "{}:{}".format(ipaddress, tcpport)
+        rx_portname = "{}_MASTER_RX".format(modbus_prefix)
+        tx_portname = "{}_MASTER_TX".format(modbus_prefix)
+
+        self.ip = AsynIP(name=port, port=dest_address,
+                         noProcessEos=True, noAutoConnect=noAutoConnect)
+        self.modbus_interpose = modbusInterpose(portname=port,
+                                                linktype=self.linktype,
+                                                timeoutmsec=2000,
+                                                writedelaymsec=0)
+        self.rx_modbus = modbusPort(portname=rx_portname, tcpportname=port,
+                                 slaveaddr=1, modbusfunction=3,
+                                 modbusstartaddr=-1, modbuslen=1,
+                                 datatype="UINT16",
+                                 pollmsec=1000,
+                                 plctype="")
+        self.tx_modbus = modbusPort(portname=tx_portname, tcpportname=port,
+                                 slaveaddr=1, modbusfunction=6,
+                                 modbusstartaddr=-1, modbuslen=1,
+                                 datatype="UINT16",
+                                 pollmsec=0,
+                                 plctype="")
+
+        self.disable = _EurothermDisable(device=device)
+        self.l1 = _EurothermL1(device=device, modbus_prefix=modbus_prefix,
+                               rregu="C/s")
+        self.__super.__init__()
+
+    ArgInfo = makeArgInfo(__init__,
+                          name=Simple("Eurotherm name", str),
+                          port=Simple("Port name", str),
+                          device=Simple("Device prefix used in PVs", str),
+                          ipaddress=Simple("Eurotherm ip address", str),
+                          tcpport=Simple("Tcp port", int),
+                          rregu=Simple("Ramp Rate units", str),
+                          modbus_prefix=Simple("Modbus prefix", str),
+                          noAutoConnect=Simple("Don't autoconnect", bool))
+
+    def Initialise(self):
+        print('asynSetOption("{port}", 0, "disconnectOnReadTimeout", "Y")'.\
+            format(port=self.port))
+
+
+class Eurotherm2K(_Eurotherm):
+    linktype = "rtu"
+
+    def __init__(self, **kwargs):
+        self.__super.__init__(**kwargs)
+        self.gui = _Eurotherm2kGui(name=self.name, device=self.device)
+        self.arch = _Eurotherm2kArchive(device=self.device)
+
+
+class Eurotherm3K(_Eurotherm):
+    linktype = "tcpip" #FYI
+
+    def __init__(self, **kwargs):
+        self.__super.__init__(**kwargs)
+        self.l2 = _EurothermL2(device=self.device,
+                               modbus_prefix=self.modbus_prefix,
+                               rregu="C/s")
+        self.prog = _EurothermProg(device=self.device,
+                                   modbus_prefix=self.modbus_prefix)
+        self.rec = _EurothermRec(device=self.device,
+                                 modbus_prefix=self.modbus_prefix)
+        self.gui = _Eurotherm3kGui(name=self.name, device=self.device)
+        self.arch = _Eurotherm3kArchive(device=self.device)
